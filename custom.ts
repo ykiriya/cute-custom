2876306cb43cd8a834165b4bcc265e0180fc8b4f
ykiriya <77214610+ykiriya@users.noreply.github.com>
2022-05-14 14:03:00 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ custom.ts
@@ -0,0 +1,63 @@
+
+
+
+/**
+ * Custom blocks
+ */
+//% weight=100 color=#0fbc11 icon="/UF076"
+//%color=#ff8800 block="中学学習"
+
+namespace Custom {
+    /**
+     * TODO: describe your function here
+     * @param n describe parameter here, eg: 2
+     * @param m describe parameter here, eg: 3
+     * @param e describe parameter here
+     */
+    //% block="前 Line       調整 $n "
+    export function foo(n: number): void {
+        let paw = 0
+        if (cuteBot.tracking(cuteBot.TrackingState.L_R_unline)) {
+            while (cuteBot.tracking(cuteBot.TrackingState.L_R_unline)) {
+                cuteBot.motors(20, 20 + paw / n)
+            }
+            if (cuteBot.tracking(cuteBot.TrackingState.L_R_line)) {
+                while (cuteBot.tracking(cuteBot.TrackingState.L_R_line) || (cuteBot.tracking(cuteBot.TrackingState.L_unline_R_line) || cuteBot.tracking(cuteBot.TrackingState.L_line_R_unline))) {
+                    cuteBot.motors(20, 20 + paw / n)
+                }
+            } else if (cuteBot.tracking(cuteBot.TrackingState.L_unline_R_line)) {
+                while (cuteBot.tracking(cuteBot.TrackingState.L_unline_R_line)) {
+                    cuteBot.motors(20, 20 + paw / n)
+                    paw += -1
+                }
+                while (cuteBot.tracking(cuteBot.TrackingState.L_R_line) || (cuteBot.tracking(cuteBot.TrackingState.L_unline_R_line) || cuteBot.tracking(cuteBot.TrackingState.L_line_R_unline))) {
+                    cuteBot.motors(20, 20 + paw / n)
+                }
+            } else {
+                while (cuteBot.tracking(cuteBot.TrackingState.L_line_R_unline)) {
+                    cuteBot.motors(20, 20 + paw / n)
+                    paw += 1
+                }
+                while (cuteBot.tracking(cuteBot.TrackingState.L_R_line) || (cuteBot.tracking(cuteBot.TrackingState.L_unline_R_line) || cuteBot.tracking(cuteBot.TrackingState.L_line_R_unline))) {
+                    cuteBot.motors(20, 20 + paw / n)
+                }
+            }
+        } else {
+            while (cuteBot.tracking(cuteBot.TrackingState.L_R_line) || (cuteBot.tracking(cuteBot.TrackingState.L_unline_R_line) || cuteBot.tracking(cuteBot.TrackingState.L_line_R_unline))) {
+                cuteBot.motors(20, 20 + paw / n)
+            }
+        }
+
+    }
+    
+    //%block="左折　　　$m "
+    export function fo3(m: number): void {
+        cuteBot.motors(-30, 30);
+        basic.pause(300 * m);
+        cuteBot.stopcar()
+        basic.pause(500);
+    }
+    
+
+
+}
---
+++ main.ts
@@ -0,0 +1,6 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 13; index++) {
+        Custom.foo(1)
+    }
+    cuteBot.stopcar()
+})
