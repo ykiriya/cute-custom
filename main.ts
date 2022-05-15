f1a9dbaf83c15d833230332c5cfd3fb21dcd77f4
ykiriya <77214610+ykiriya@users.noreply.github.com>
2022-05-16 07:31:40 +0900

    更新 pxt.json, main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,3 +1,75 @@
+let n = 0
+let paw = 0
 input.onButtonPressed(Button.A, function () {
-	
+    n = 2
+    for (let index = 0; index < 4; index++) {
+        if (cuteBot.tracking(cuteBot.TrackingState.L_R_unline)) {
+            while (cuteBot.tracking(cuteBot.TrackingState.L_R_unline)) {
+                cuteBot.motors(20, 20 + paw / n)
+            }
+            if (cuteBot.tracking(cuteBot.TrackingState.L_R_line)) {
+                while (cuteBot.tracking(cuteBot.TrackingState.L_R_line) || (cuteBot.tracking(cuteBot.TrackingState.L_unline_R_line) || cuteBot.tracking(cuteBot.TrackingState.L_line_R_unline))) {
+                    cuteBot.motors(20, 20 + paw / n)
+                }
+            } else if (cuteBot.tracking(cuteBot.TrackingState.L_unline_R_line)) {
+                while (cuteBot.tracking(cuteBot.TrackingState.L_unline_R_line)) {
+                    cuteBot.motors(20, 20 + paw / n)
+                    paw += -1
+                }
+                while (cuteBot.tracking(cuteBot.TrackingState.L_R_line) || (cuteBot.tracking(cuteBot.TrackingState.L_unline_R_line) || cuteBot.tracking(cuteBot.TrackingState.L_line_R_unline))) {
+                    cuteBot.motors(20, 20 + paw / n)
+                }
+            } else {
+                while (cuteBot.tracking(cuteBot.TrackingState.L_line_R_unline)) {
+                    cuteBot.motors(20, 20 + paw / n)
+                    paw += 1
+                }
+                while (cuteBot.tracking(cuteBot.TrackingState.L_R_line) || (cuteBot.tracking(cuteBot.TrackingState.L_unline_R_line) || cuteBot.tracking(cuteBot.TrackingState.L_line_R_unline))) {
+                    cuteBot.motors(20, 20 + paw / n)
+                }
+            }
+        } else {
+            while (cuteBot.tracking(cuteBot.TrackingState.L_R_line) || (cuteBot.tracking(cuteBot.TrackingState.L_unline_R_line) || cuteBot.tracking(cuteBot.TrackingState.L_line_R_unline))) {
+                cuteBot.motors(20, 20 + paw / n)
+            }
+        }
+    }
+})
+function 前進_Line (数値: number) {
+    if (cuteBot.tracking(cuteBot.TrackingState.L_R_unline)) {
+        while (cuteBot.tracking(cuteBot.TrackingState.L_R_unline)) {
+            cuteBot.motors(20, 20 + paw / n)
+        }
+        if (cuteBot.tracking(cuteBot.TrackingState.L_R_line)) {
+            while (cuteBot.tracking(cuteBot.TrackingState.L_R_line) || (cuteBot.tracking(cuteBot.TrackingState.L_unline_R_line) || cuteBot.tracking(cuteBot.TrackingState.L_line_R_unline))) {
+                cuteBot.motors(20, 20 + paw / n)
+            }
+        } else if (cuteBot.tracking(cuteBot.TrackingState.L_unline_R_line)) {
+            while (cuteBot.tracking(cuteBot.TrackingState.L_unline_R_line)) {
+                cuteBot.motors(20, 20 + paw / n)
+                paw += -1
+            }
+            while (cuteBot.tracking(cuteBot.TrackingState.L_R_line) || (cuteBot.tracking(cuteBot.TrackingState.L_unline_R_line) || cuteBot.tracking(cuteBot.TrackingState.L_line_R_unline))) {
+                cuteBot.motors(20, 20 + paw / n)
+            }
+        } else {
+            while (cuteBot.tracking(cuteBot.TrackingState.L_line_R_unline)) {
+                cuteBot.motors(20, 20 + paw / n)
+                paw += 1
+            }
+            while (cuteBot.tracking(cuteBot.TrackingState.L_R_line) || (cuteBot.tracking(cuteBot.TrackingState.L_unline_R_line) || cuteBot.tracking(cuteBot.TrackingState.L_line_R_unline))) {
+                cuteBot.motors(20, 20 + paw / n)
+            }
+        }
+    } else {
+        while (cuteBot.tracking(cuteBot.TrackingState.L_R_line) || (cuteBot.tracking(cuteBot.TrackingState.L_unline_R_line) || cuteBot.tracking(cuteBot.TrackingState.L_line_R_unline))) {
+            cuteBot.motors(20, 20 + paw / n)
+        }
+    }
+}
+input.onButtonPressed(Button.B, function () {
+    n = 2
+    for (let index = 0; index < 4; index++) {
+        前進_Line(n)
+    }
 })
